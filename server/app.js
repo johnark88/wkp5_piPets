60fdead34ba430619fbe7ce9ffa0975181814647
John Arkema <john.arkema@gmail.com>
2016-09-24 17:27:15 -0500

    index route

--- server/app.js
+++ server/app.js
@@ -4,8 +4,9 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var app = express();
 var portDecision = process.env.PORT || 8081;
+var index = require('./routes/index');
 
-//json body parser 
+//json body parser
 app.use(bodyParser.json());
 
 //listen and port decision
@@ -13,12 +14,9 @@ app.listen(portDecision,function(){
 console.log('Listening my good sir ', portDecision);
 });
 
-//app.get --  serves the index -- get params
-app.get("/*", function(req,res){
-    console.log("Here is the property: ", req.params[0]);
-    var file = req.params[0] || "index.html";
-    res.sendFile(path.join(__dirname, "../public", file));
-});
+//routes
+app.use('/', index);
+
 
 //data base connection
 var mongoURI = "mongodb://localhost:27017/assignments";
