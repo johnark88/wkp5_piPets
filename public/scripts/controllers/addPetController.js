b4c2f73284a82d85da316462db829e5022d55d5b
John Arkema <john.arkema@gmail.com>
2016-09-25 16:04:10 -0500

    add a pet works

--- public/scripts/controllers/addPetController.js
+++ public/scripts/controllers/addPetController.js
@@ -1,5 +1,21 @@
-myApp.controller('addPetController', ['$scope', function($scope){
+myApp.controller('addPetController', ['$scope', '$http' ,function($scope, $http){
 console.log('addPet is here');
 
+$scope.addNewPet = function(){
+var newPet = {
+  pet_name:$scope.petName,
+  animal:$scope.animalType,
+  age:$scope.petAge,
+  petPicURL:$scope.petPic
+};//end newPet object
 
-}]);
+$http ({
+  method: 'POST',
+  url: '/savePet',
+  data: newPet
+}).then(function(response){
+  console.log(response,'from server');
+
+});//end then function
+};//end addNewPet function
+}]);//end add pet controller
