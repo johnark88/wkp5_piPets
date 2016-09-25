fc13083c37026bbb9f474c15b8c76d345aa74ced
John Arkema <john.arkema@gmail.com>
2016-09-25 11:55:34 -0500

    routes still cannot get/post

--- server/routes/allPets.js
+++ server/routes/allPets.js
@@ -1,15 +1,19 @@
-// var router = require('express').Router();
-//
-// router.get('/viewAll', function(req,res){
-// console.log('in get all router ');
-// if (err) {
-//   console.log(err);
-// }else {
-//   console.log('Connected to db!');
-//   res.send(allPets);
-//   console.log(allPets);
-// }
-// });
-//
-//
-// module.exports = router;
+var router = require('express').Router();
+
+
+
+router.get('/viewAll', function (req,res){
+console.log('in get all router ');
+newPet.find({}, function(err, piPetResults) {
+    if(err){
+      console.log('error occurred:', err);
+      res.sendStatus(500);
+    }else{
+      res.send(piPetResults);
+      console.log(piPetResults);
+    }
+  });
+});
+
+
+module.exports = router;
