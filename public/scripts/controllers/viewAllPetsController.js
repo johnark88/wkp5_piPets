1c1b5925c9351a919cea391ff2f8904f7ca311f5
John Arkema <john.arkema@gmail.com>
2016-09-25 14:06:06 -0500

    still trying to display on DOM

--- public/scripts/controllers/viewAllPetsController.js
+++ public/scripts/controllers/viewAllPetsController.js
@@ -3,12 +3,12 @@ console.log('viewAllPets is here');
 
 //if I want to add a button I would need this
 // $scope.getpiPets = function (){
-
+$scope.allPiPets=[];
 $http ({
   method: 'GET',
   url:'/viewAll'
 }).then(function(response){
-$scope.allPiPets = response;
+$scope.allPiPets.push(response);
 console.log($scope.allPiPets);
 });
 // };//end scope getpiPets function
