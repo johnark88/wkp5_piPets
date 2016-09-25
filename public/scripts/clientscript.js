bb9e6614420a6bc6cf43c175c5319e618b45fe48
John Arkema <john.arkema@gmail.com>
2016-09-25 12:34:10 -0500

    Angular Routing

--- public/scripts/clientscript.js
+++ public/scripts/clientscript.js
@@ -2,7 +2,21 @@ console.log('js sourced');
 
 var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.controller('newPet',['$scope', '$http',function($scope,$http){
+myApp.controller('piPetsHome',['$scope',function($scope){
   console.log('NG');
 
 }]);
+myApp.config(['$routeProvider',function ($routeProvider){
+    $routeProvider.
+    when ("/addPet",
+      {templateUrl: "partials/addPet.html",
+      controller: "addPetController"
+    }).
+    when ("/viewAllPets",
+      {templateUrl: "partials/viewAllPets.html",
+      controller: "viewAllPetsController"
+    }).
+    otherwise({
+      redirectTo: "/"
+    });
+  }]);
---
+++ public/scripts/controllers/addPetController.js
@@ -0,0 +1,5 @@
+myApp.controller('addPet', ['$scope', function($scope){
+console.log('addPet is here');
+
+
+}]);
