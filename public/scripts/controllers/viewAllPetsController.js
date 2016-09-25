a2049b309b605c00a41392426dc3fb03cc934665
John Arkema <john.arkema@gmail.com>
2016-09-25 15:18:12 -0500

    display all with picture

--- public/scripts/controllers/viewAllPetsController.js
+++ public/scripts/controllers/viewAllPetsController.js
@@ -1,15 +1,13 @@
 myApp.controller('viewAllPetsController', ['$scope','$http' ,function($scope, $http){
-console.log('viewAllPets is here');
+  console.log('viewAllPets is here');
 
-//if I want to add a button I would need this
-// $scope.getpiPets = function (){
-$scope.allPiPets=[];
-$http ({
-  method: 'GET',
-  url:'/viewAll'
-}).then(function(response){
-$scope.allPiPets.push(response);
-console.log($scope.allPiPets);
-});
-// };//end scope getpiPets function
+  $scope.allPiPets=[];
+  $http ({
+    method: 'GET',
+    url:'/viewAll'
+  }).then(function(response){
+    $scope.allPiPets = response.data;
+    console.log($scope.allPiPets);
+  });
+
 }]);//end viewAllPetsController
