2e1c6e99203608ff0f54f153f121ccf82985dc38
John Arkema <john.arkema@gmail.com>
2016-09-25 13:51:34 -0500

    trying to display response from db

--- server/routes/allPets.js
+++ server/routes/allPets.js
@@ -1,19 +1,19 @@
-var router = require('express').Router();
-
-
-
-router.get('/viewAll', function (req,res){
-console.log('in get all router ');
-newPet.find({}, function(err, piPetResults) {
-    if(err){
-      console.log('error occurred:', err);
-      res.sendStatus(500);
-    }else{
-      res.send(piPetResults);
-      console.log(piPetResults);
-    }
-  });
-});
-
-
-module.exports = router;
+// var router = require('express').Router();
+//
+//
+//
+// router.get('/viewAll', function (req,res){
+// console.log('in get all router ');
+// newPet.find({}, function(err, piPetResults) {
+//     if(err){
+//       console.log('error occurred:', err);
+//       res.sendStatus(500);
+//     }else{
+//       res.send(piPetResults);
+//       console.log(piPetResults);
+//     }
+//   });
+// });
+//
+//
+// module.exports = router;
