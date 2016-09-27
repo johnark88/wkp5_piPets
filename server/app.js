5947779f4fc604a5dc6beb02e5593468160af3a9
John Arkema <john.arkema@gmail.com>
2016-09-26 19:34:41 -0500

    fixed on success 200

--- server/app.js
+++ server/app.js
@@ -6,19 +6,16 @@ var path = require('path');
 var app = express();
 var portDecision = process.env.PORT || 8081;
 
-// routes
-var index = require('./routes/index');
-// var allPets = require('./routes/allPets');
-// var savePets = require('./routes/savePets');
-
 
 //json body parser
 app.use(bodyParser.json());
 
-// //routes
+// routes
+// var allPets = require('./routes/allPets');
+// var savePets = require('./routes/savePets');
+var index = require('./routes/index');
 app.use('/', index);
-// app.use('/viewAll', allPets)
-// app.use('/savePets', savePets);
+
 
 //data base connection
 var mongoURI = "mongodb://localhost:27017/piPets";
@@ -27,18 +24,13 @@ mongoose.connect(mongoURI);
 
 //listen and port decision
 app.listen(portDecision,function(){
-console.log('Listening my good sir ', portDecision);
+  console.log('Listening my good sir ', portDecision);
 });
-
-// // app.get --  serves the index -- get params
-// app.get( '/', function( req, res ){
-// res.sendFile( path.resolve( 'public/index.html' ) );
-// });
 
 //app.get view all
 app.get('/viewAll', function (req,res){
-console.log('in get all router ');
-newPet.find({}, function(err, piPetResults) {
+  console.log('in get all router ');
+  newPet.find({}, function(err, piPetResults) {
     if(err){
       console.log('error occurred:', err);
       res.sendStatus(500);
@@ -49,8 +41,10 @@ newPet.find({}, function(err, piPetResults) {
   });//end database query
 });//end app.get
 
+//addPet Model
 var newPet = require('../models/addPet');
 
+//app.post for save pet
 app.post('/savePet', function(req,res){
   console.log('in add pet post route');
 
@@ -58,30 +52,29 @@ app.post('/savePet', function(req,res){
   console.log('piPet : ', req.body);
 
   piPet.save(function(err){
-  if (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }else {
-    console.log('connected to the db!');
-    res.sendStatus(20);
-  }//end else
-});//end save pet to database
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }else {
+      console.log('connected to the db!');
+      res.sendStatus(201);
+    }//end else
+  });//end save pet to database
 });//end app.post
 
-
+//app delete function
 app.delete('/deletePet/:id', function(req,res){
-
   console.log("params = ", req.params);
-     newPet.remove({"_id": req.params.id}, function(err) {
-       if(err){
-         console.log('error occurred:', err);
-         res.sendStatus(500);
-       }else{
-         console.log('removed=', req.params.id);
-         res.sendStatus(200);
-       }
-     });
- });
+  newPet.remove({"_id": req.params.id}, function(err) {
+    if(err){
+      console.log('error occurred:', err);
+      res.sendStatus(500);
+    }else{
+      console.log('removed=', req.params.id);
+      res.sendStatus(200);
+    }
+  });
+});
 
 // //use this for index.html
 app.use( express.static( 'public' ) );
